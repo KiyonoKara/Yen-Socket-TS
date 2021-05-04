5693e60c5f5c0f6f6f57d64d5852e18f9c630799
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 11:24:23 -0700

    Create constructor

--- src/util/InitializeHeaders.ts
+++ src/util/InitializeHeaders.ts
@@ -1,1 +1,12 @@
 import { URL } from "url";
+
+interface Options {
+    [key: string]: any;
+}
+
+export class InitializeHeaders {
+    constructor(public options: Partial<Options> = {}) {
+        this.options = options || {};
+    }
+
+}
