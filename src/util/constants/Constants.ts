55c720834a24ddbf0cc7549f748cfe0061b27a49
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 12:22:04 -0700

    More constants: default port which is 443, latest ws version 13, upgrade header value that is just called "upgrade"

--- src/util/constants/Constants.ts
+++ src/util/constants/Constants.ts
@@ -1,2 +1,13 @@
-export const GUID: String = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-export const BASE_BUFFER: Buffer = Buffer.alloc(0);
+const GUID: String = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+const BASE_BUFFER: Buffer = Buffer.alloc(0);
+const DEFAULT_PORT: number = 443;
+const WS_VERSION: number = 13;
+const UPGRADE: string = "upgrade";
+
+export {
+  GUID,
+  BASE_BUFFER,
+  DEFAULT_PORT,
+  WS_VERSION,
+  UPGRADE
+};
