c6f9bb49d427e04ceca9e426f45c12272ab3ec87
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 12:05:20 -0700

    Added detailed documentation

--- src/util/GenerateKey.ts
+++ src/util/GenerateKey.ts
@@ -1,10 +1,21 @@
 import * as crypto from "crypto";
 import { GUID } from "./constants/Constants";
 
+/**
+ * Generate a sec WebSocket key with a default size of 16 and an encoding of base64
+ * @param size - Preferably 16
+ * @param encoding - Base64 is preferred but other encodings are doable
+ */
 export function generateSecWebSocketKey(size: number = 16, encoding: BufferEncoding = "base64"): string {
     return crypto.randomBytes(size).toString(encoding);
 }
 
+/**
+ * Create an expected key that is compared with the upgraded WebSocket connection headers
+ * @param {string} algorithm - Hash algorithm
+ * @param {any} encoding - Uses 'any' type to cast to the BinaryToTextEncoding type
+ * @param secWebSocketKey - Use the sec-WebSocket key or it will generate one on its own
+ */
 export function createExpectedKey(algorithm: string = "sha1", encoding: any = "base64", secWebSocketKey: string = generateSecWebSocketKey()): string {
     return crypto
         .createHash(algorithm)
@@ -12,5 +23,3 @@ export function createExpectedKey(algorithm: string = "sha1", encoding: any = "b
         .digest(encoding)
         ?.toString();
 }
-
-
