9810385dfc506a74cd33323ffe5ef5f43d92c89e
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 12:22:53 -0700

    Fixed upgrade header value

--- src/util/constants/Constants.ts
+++ src/util/constants/Constants.ts
@@ -2,7 +2,7 @@ const GUID: String = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 const BASE_BUFFER: Buffer = Buffer.alloc(0);
 const DEFAULT_PORT: number = 443;
 const WS_VERSION: number = 13;
-const UPGRADE: string = "upgrade";
+const UPGRADE: string = "Upgrade";
 
 export {
   GUID,
