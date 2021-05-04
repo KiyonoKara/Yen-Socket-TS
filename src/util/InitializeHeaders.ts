86c1a96d17eaae010d15a6dd8117ad939c6dcec9
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 12:23:42 -0700

    Header var

--- src/util/InitializeHeaders.ts
+++ src/util/InitializeHeaders.ts
@@ -1,5 +1,6 @@
 import { URL } from "url";
 import * as GenerateKey from "./GenerateKey";
+import * as Constants from "./constants/Constants";
 
 interface Options {
     [key: string]: any;
@@ -10,5 +11,17 @@ export class InitializeHeaders {
     constructor(public options: Partial<Options> = {}) {
         this.options = options || {};
         this.WSKey = GenerateKey.generateSecWebSocketKey(16, "base64");
+    }
+
+    initializeHeaders(url: string = this.options.url) {
+        const wsURL: URL = new URL(url);
+
+        const headers = {
+            Host: `${wsURL.host}:${wsURL.port || Constants.DEFAULT_PORT}`,
+            Connection: Constants.UPGRADE,
+            Upgrade: "websocket",
+            "Sec-WebSocket-Key":this.WSKey,
+            "Sec-WebSocket-Version": Constants.WS_VERSION
+        };
     }
 }
