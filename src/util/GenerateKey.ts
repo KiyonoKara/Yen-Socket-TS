f25fab7b955af919fd6e23bbc9a21750260679d5
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 12:02:30 -0700

    Creating an expected sec websocket key generator

--- src/util/GenerateKey.ts
+++ src/util/GenerateKey.ts
@@ -4,3 +4,13 @@ import { GUID } from "./constants/Constants";
 export function generateSecWebSocketKey(size: number = 16, encoding: BufferEncoding = "base64"): string {
     return crypto.randomBytes(size).toString(encoding);
 }
+
+export function createExpectedKey(algorithm: string = "sha1", encoding: any = "base64", secWebSocketKey: string = generateSecWebSocketKey()): string {
+    return crypto
+        .createHash(algorithm)
+        .update(`${secWebSocketKey}${GUID}`)
+        .digest(encoding)
+        ?.toString();
+}
+
+
