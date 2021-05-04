9c9448717a09cb1a30ce8c6fbcedea9eb79fd876
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 12:08:45 -0700

    Class constant for the generated WebSocket key

--- src/util/InitializeHeaders.ts
+++ src/util/InitializeHeaders.ts
@@ -1,12 +1,14 @@
 import { URL } from "url";
+import * as GenerateKey from "./GenerateKey";
 
 interface Options {
     [key: string]: any;
 }
 
 export class InitializeHeaders {
+    declare WSKey: string;
     constructor(public options: Partial<Options> = {}) {
         this.options = options || {};
+        this.WSKey = GenerateKey.generateSecWebSocketKey(16, "base64");
     }
-
 }
