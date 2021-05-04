4aa59b3077676fd52c63ec28cb85576c1ada5a64
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 11:34:57 -0700

    Implemented generator for WS key

--- src/util/GenerateKey.ts
+++ src/util/GenerateKey.ts
@@ -1,2 +1,6 @@
 import * as crypto from "crypto";
+import { GUID } from "./constants/Constants";
 
+export function generateSecWebSocketKey(size: number = 16, encoding: BufferEncoding = "base64"): string {
+    return crypto.randomBytes(size).toString(encoding);
+}
