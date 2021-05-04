7d78cb05f5bd9c3c44ad09b0a0e2958fd0dec6f9
KaNguy <68307546+KaNguy@users.noreply.github.com>
2021-05-04 14:28:34 -0700

    Building the class with validation

--- src/connect/YenSocketTS.ts
+++ src/connect/YenSocketTS.ts
@@ -1,2 +1,37 @@
 import * as https from "https";
+import * as http from "http";
+import * as http2 from "http2";
+import { EventEmitter } from "events";
 import { InitializeHeaders } from "../util/InitializeHeaders";
+import { Options } from "../util/Interfaces";
+import { createExpectedKey } from "../util/GenerateKey";
+
+const IH = new InitializeHeaders();
+
+class YenSocketTS extends EventEmitter {
+    declare url: string;
+    declare request: http.ClientRequest;
+
+    constructor(url, public options: Partial<Options> = {}) {
+        super(url);
+
+        this.options = options || {};
+
+        const headersWS = IH.initializeHeaders(url);
+        const WSOptions = IH.createOptions(url, headersWS);
+
+        this.request = https.request(WSOptions);
+
+        this.request.on('upgrade', (response: http.IncomingMessage, socket: any, head: Buffer) => {
+            // Ensure WebSocket connection has the sec-websocket-accept header
+            if (!response.headers["sec-websocket-accept"]) {
+                throw new Error("The sec-websocket-accept header is missing.");
+            }
+            // Validate the server key
+            const expectedKey = createExpectedKey("sha1", "base64", IH.WSKey);
+            if (response.headers["sec-websocket-accept"] !== expectedKey) {
+                throw new Error("The sec-websocket-accept header returned a mismatched key.");
+            }
+        });
+    }
+}
